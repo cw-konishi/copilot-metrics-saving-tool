7b7280fa99b09a4a3f741d6f57b0c975e7348ea0
cw-konishi <kota.konishi.ey@nttcom.co.jp>
2025-02-07 13:21:11 +0900

    トータル承認行数のデータの追加

--- backend/main.py
+++ backend/main.py
@@ -81,8 +81,12 @@ def convert_to_prometheus_format(metrics_data):
                     language_name = language['name']
                     total_code_suggestions = language['total_code_suggestions']
                     total_code_acceptances = language['total_code_acceptances']
+                    total_code_lines_suggestions = language['total_code_lines_suggestions']
+                    total_code_lines_accepted = language['total_code_lines_accepted']
                     lines.append(f'total_code_suggestions{{date="{date}", editor="{editor_name}", model="{model_name}", language="{language_name}"}} {total_code_suggestions}')
                     lines.append(f'total_code_acceptances{{date="{date}", editor="{editor_name}", model="{model_name}", language="{language_name}"}} {total_code_acceptances}')
+                    lines.append(f'total_code_lines_suggestions{{date="{date}", editor="{editor_name}", model="{model_name}", language="{language_name}"}} {total_code_lines_suggestions}')
+                    lines.append(f'total_code_lines_accepted{{date="{date}", editor="{editor_name}", model="{model_name}", language="{language_name}"}} {total_code_lines_accepted}')
         
         # Copilot IDE Chat
         for editor in metric['copilot_ide_chat']['editors']:
